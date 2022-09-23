fe727f6d883f315fb7a2a537774909f33fda4aab
Yuvraj Maharshi <yogeshnda2018@gmail.com.com>
2022-09-23 19:57:08 +0530

    added prime and palindrome problem

---
+++ palindrome.js
@@ -0,0 +1,12 @@
+ const revVlaue=() =>{
+      let myrevVal=str.toString().split("").reverse().join("");
+      let mystrVal=str.toString;
+   
+       if(mystrVal==myrevVal){
+          console.log("Yes");
+      }else{
+          console.log("No");
+      }
+  };
+ 
+  revVlaue();
---
+++ prime.js
@@ -0,0 +1,14 @@
+ function checkprime(num){let factors=0;
+for(let i=1; i<=num; i++){
+  if(num%i==0){
+    factors++
+  }
+}
+ if(factors==i){
+   return ;
+} return false;  }
+for (i=2; i<=9; i++){
+  let ans=checkprime(i);
+  if(ans==false){console.log(i,"is not a primenumber")
+  }else {console.log(i,"is a prime number")} 
+}
